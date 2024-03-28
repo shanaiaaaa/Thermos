ed30a63c58b3df3755c7d4b9f6772d39bcca5261
Aaron Allan Fernandes <shanaia0213@gmail.com>
2024-03-28 11:27:26 +0530

    second commit

--- project/src/prac/thermos.java
+++ project/src/prac/thermos.java
@@ -3,23 +3,34 @@ package prac;
 public class thermos {
 	String Brand="Godrej";
 	String Liquid="Orange Juice";
+	int Capacity= 1000;
+	int volume;
 
 	thermos(){
 		this.Brand=Brand;
 		this.Liquid=Liquid;
+		this.volume=0;
 	}
-	 void addliquid(){
+
+	 void addliquid() {
 		 
-		 Liquid="Tea";
 		 
-	 }
-	 
-	 void addbrand() {
-		 Brand="Milton";
+		 int c=200; //enter capacity in m
+		 
+		 if(Capacity>c) {
+			 Capacity=Capacity-c;
+			 System.out.println(Capacity+"ml of "+Liquid+" added to the thermos");
+		 }else if(Capacity==0) {
+			 System.out.println("Thermos is full");
+		 }
+		 else {
+			 System.out.println("liquid will overflow"); 
+		 }
+		 
+		 
 	 }
 	 
 	 void display() {
-		 System.out.println("The Brand of thermos is "+Brand);
-		 System.out.println("The Liquid in the thermos is "+Liquid);
+		System.out.println("Brand: "+Brand);
 	 }
 }
